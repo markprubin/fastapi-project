cdf5c4c0018f91e0f239524f3ae337b45fde9b26
Mark Rubin <markrubin@atventurepartners.tech>
2023-12-01 11:28:18 -0700

    adds more routes

--- main.py
+++ main.py
@@ -20,7 +20,12 @@ async def get_todos():
     return {"todos": todos}
 
 # get single todo
-
+@app.get("/todos/{todo_id}")
+async def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message": "No todos found"}
 
 # Create a todo
 @app.post("/todos")
@@ -29,7 +34,21 @@ async def create_todos(todo: Todo):
     return {"message": "Todo added!"}
 
 # update a todo
-
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: int, todo_obj: Todo):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.id = todo_id
+            todo.item = todo_obj.item
+            return {"todo": todo}
+    return {"message": "No todo's found to update"}
 
 # delete a todo
 
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return {"message": "Todo removed"}
+    return {"message": "No todo found"}
