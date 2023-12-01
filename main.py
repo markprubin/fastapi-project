bfb522c2830a953feed3b1a36b7f6bdc722a26db
Mark Rubin <markrubin@atventurepartners.tech>
2023-12-01 11:02:05 -0700

    creates model, adds get and post routes for todo app

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from models import Todo
+
+
 
 app = FastAPI()
 
@@ -6,4 +9,27 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+todos = []
+
+# Get all todos
+
+@app.get("/todos")
+async def get_todos():
+    return {"todos": todos}
+
+# get single todo
+
+
+# Create a todo
+@app.post("/todos")
+async def create_todos(todo: Todo):
+    todos.append(todo)
+    return {"message": "Todo added!"}
+
+# update a todo
+
+
+# delete a todo
 
